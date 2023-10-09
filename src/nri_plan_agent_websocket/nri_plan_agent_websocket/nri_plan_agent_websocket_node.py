b0bec5fe13be5d0b2e5994eab9f79ac64a70209a
Kangneoung Lee <eehoa12381@gmail.com>
2023-10-09 14:25:35 -0500

    new topic for auto crop field search mode

--- src/nri_plan_agent_websocket/nri_plan_agent_websocket/nri_plan_agent_websocket_node.py
+++ src/nri_plan_agent_websocket/nri_plan_agent_websocket/nri_plan_agent_websocket_node.py
@@ -5,11 +5,11 @@
 import rclpy 
 import roslibpy
 from rclpy.node import Node 
-from std_msgs.msg import Float64, Int8
+from std_msgs.msg import Float64, Int8, Float64MultiArray
 
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
-
+from nri_plan_msgs.msg import Gps3WayPoints
 
 import os
 import math
@@ -40,10 +40,14 @@ class NriPlanAgWebSocket(Node):
     self.robot_gps_goal_to_agent_topic = self.get_parameter('robot_gps_goal_to_agent_topic').get_parameter_value().string_value
     self.robot_gps_from_agent_topic = self.get_parameter('robot_gps_from_agent_topic').get_parameter_value().string_value
     
+    self.robot_automode_to_agent_topic = 'auto_mode'
+    
     self.robot_ip_list = []
     self.client_list = []
     self.gps_goal_pub_to_agent_list = []
+    self.automode_pub_to_agent_list = []
     self.gps_goal_sub_from_machine_list = []
+    self.gps3p_goal_sub_from_machine_list = []
     self.gps_pub_to_machine_list = []
     self.gps_sub_from_agent_list = []
     
@@ -59,38 +63,62 @@ class NriPlanAgWebSocket(Node):
       gps_goal_pub_to_agent = roslibpy.Topic(client, self.robot_gps_goal_to_agent_topic, 'sensor_msgs/NavSatFix')
       self.gps_goal_pub_to_agent_list.append(gps_goal_pub_to_agent)
       
+      gps_automode_pub_to_agent = roslibpy.Topic(client, self.robot_automode_to_agent_topic, 'std_msgs/Float64MultiArray')
+      self.automode_pub_to_agent_list.append(gps_automode_pub_to_agent)
+      
       gps_goal_sub_from_machine_topic = 'Robot' + str(i) + '/' + self.robot_gps_goal_topic
+      gps3p_goal_sub_from_machine_topic = 'Robot' + str(i) + '/' + self.robot_gps_goal_topic + '_3p'
 
       if i == 0:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 0), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 0), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 1:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 1), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 1), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 2:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 2), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 2), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 3:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 3), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 3), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 4:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 4), 3)      
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 4), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 5:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 5), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 5), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 6:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 6), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 6), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 7:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 7), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 7), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 8:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 8), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 8), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       elif i == 9:
         gps_goal_sub_from_machine = self.create_subscription(NavSatFix, gps_goal_sub_from_machine_topic, lambda msg: self.gps_goal_sub_fm_callback(msg, 9), 3)
         self.gps_goal_sub_from_machine_list.append(gps_goal_sub_from_machine)
+        gps3p_goal_sub_from_machine = self.create_subscription(Gps3WayPoints, gps3p_goal_sub_from_machine_topic, lambda msg: self.gps3p_goal_sub_fm_callback(msg, 9), 3)
+        self.gps3p_goal_sub_from_machine_list.append(gps3p_goal_sub_from_machine)
       
       gps_pub_to_machine_topic = 'Robot' + str(i) + '/' + self.robot_gps_topic
       gps_pub_to_machine = self.create_publisher(NavSatFix, gps_pub_to_machine_topic, 1)
@@ -118,6 +146,12 @@ class NriPlanAgWebSocket(Node):
   def timer_callback(self):
     self.try_connection()
   
+  def gps3p_goal_sub_fm_callback(self, msg, robot_index):
+    automode_pub_to_agent = self.automode_pub_to_agent_list[robot_index]
+    if self.client_list[robot_index].is_connected is True:
+      automode_pub_to_agent.publish(roslibpy.Message({'data': [1.0, msg.latitudes[0], msg.longitudes[0], msg.latitudes[1], msg.longitudes[1], msg.latitudes[2], msg.longitudes[2], 0.5]
+                                                      }))
+
   def gps_goal_sub_fm_callback(self, msg, robot_index):
     gps_goal_lat = msg.latitude
     gps_goal_long = msg.longitude
